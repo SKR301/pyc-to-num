e6124c09baf37c7bf6e154bfdb01dbc2c8346ed6
SKR301 <chunnu.bihat@gmail.com>
2022-06-02 20:33:20 +0530

    git modified the mask size

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 
-maskHeight, maskWidth = 10, 8
+maskHeight, maskWidth = 8, 6
 
-img = cv2.imread('img.png', 0)
+img = cv2.imread('img2.png', 0)         # enter path to image here
 imgHeight, imgWidth = img.shape
 img = cv2.bitwise_not(img)
 
